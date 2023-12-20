3a6e99cb7d51717413cb071483cb21df32741d9c
Maryam Ahmed <maryam-Mac@users.noreply.github.com>
2023-12-20 14:13:21 -0500

    rm

--- target/debug/deps/practice-cea117408ea6053f.d
+++
@@ -1,5 +0,0 @@
-/Users/user/Desktop/Rowan PhD Data Science/RUST/projects/variables/practice/target/debug/deps/practice-cea117408ea6053f: src/main.rs
-
-/Users/user/Desktop/Rowan PhD Data Science/RUST/projects/variables/practice/target/debug/deps/practice-cea117408ea6053f.d: src/main.rs
-
-src/main.rs:
--- target/debug/practice.d
+++
@@ -1,1 +0,0 @@
-/Users/user/Desktop/Rowan\ PhD\ Data\ Science/RUST/projects/variables/practice/target/debug/practice: /Users/user/Desktop/Rowan\ PhD\ Data\ Science/RUST/projects/variables/practice/src/main.rs
